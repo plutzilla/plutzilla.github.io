de79e7a1ea197239dd5d7c60b79cc91c6af4eb41
Paulius Leščinskas <paulius.lescinskas@gmail.com>
2015-11-18 21:27:09 +0200

    RSS redirect option

--- 404.php
+++ 404.php
@@ -1,5 +1,11 @@
 <?php
 
+$redirectUri = 'http://old.lescinskas.lt'.$_SERVER['REQUEST_URI'];
+
+if($_SERVER['REQUEST_URI'] == '/lt/blog/rss' || $_SERVER['REQUEST_URI'] == '/index.php/lt/blog/rss') {
+	$redirectUri = '/feed.xml';
+}
+
 header('HTTP/1.1 301 Moved Permanently');
-header('Location: http://old.lescinskas.lt'.$_SERVER['REQUEST_URI']);
+header('Location: '.$redirectUri);
 exit;
