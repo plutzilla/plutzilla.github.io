287b1a3184c99c86fde9c12cf73a2370d061f31a
Paulius Leščinskas <paulius.lescinskas@gmail.com>
2019-09-15 19:12:40 +0300

    Changed image push command

--- build/build.sh
+++ build/build.sh
@@ -4,7 +4,7 @@ docker run -v $(dirname "$(pwd)"):/site -e JEKYLL_ENV=production plutzilla/lesci
 sudo chown -R paulius:paulius ../_site
 gulp -f ../gulpfile.js
 docker build -t eu.gcr.io/lescinskas-lt/nginx-server:latest -t eu.gcr.io/lescinskas-lt/nginx-server:v4 -f ../nginx.Dockerfile ..
-gcloud docker -- push eu.gcr.io/lescinskas-lt/nginx-server:latest
-gcloud docker -- push eu.gcr.io/lescinskas-lt/nginx-server:v4
+docker push eu.gcr.io/lescinskas-lt/nginx-server:latest
+docker push eu.gcr.io/lescinskas-lt/nginx-server:v4
 # TODO Deploy to k8s
 # kubectl apply -f ./k8s/deploy-beta.yml
