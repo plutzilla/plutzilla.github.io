cfde204b5379d810606ee2b9d07524edd77eb41b
Paulius Leščinskas <Paulius.Lescinskas@gmail.com>
2019-10-29 17:58:52 +0200

    New blogpost: moving to HTTPS

--- build/build.sh
+++ build/build.sh
@@ -3,9 +3,9 @@
 docker run -v $(dirname "$(pwd)"):/site -e JEKYLL_ENV=production plutzilla/lescinskas.lt:jekyll build
 sudo chown -R paulius:paulius ../_site
 gulp -f ../gulpfile.js
-docker build -t eu.gcr.io/lescinskas-lt/nginx-server:latest -t eu.gcr.io/lescinskas-lt/nginx-server:v8 -f ../nginx.Dockerfile ..
+docker build -t eu.gcr.io/lescinskas-lt/nginx-server:latest -t eu.gcr.io/lescinskas-lt/nginx-server:v9 -f ../nginx.Dockerfile ..
 docker push eu.gcr.io/lescinskas-lt/nginx-server:latest
-docker push eu.gcr.io/lescinskas-lt/nginx-server:v8
+docker push eu.gcr.io/lescinskas-lt/nginx-server:v9
 # TODO Deploy to k8s
 # kubectl apply -f ./k8s/deploy-beta.yml
 # kubectl apply -f ./k8s/deploy-www.yml
