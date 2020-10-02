535becef6c707700e3dca8498e0e1414fbc572ca
Paulius Leščinskas <paulius.lescinskas@gmail.com>
2020-10-02 20:11:11 +0300

    Namespace update in the descheduler deployment script

--- deploy/helm/descheduler.sh
+++ deploy/helm/descheduler.sh
@@ -4,5 +4,7 @@
 # https://github.com/kubernetes-sigs/descheduler
 # Helm chart: https://github.com/kubernetes-sigs/descheduler/blob/master/charts/descheduler/README.md
 # Helm values:     https://github.com/kubernetes-sigs/descheduler/blob/master/charts/descheduler/values.yaml
+
+# Descheduler must be installed in kube-system namespace
 helm repo add descheduler https://kubernetes-sigs.github.io/descheduler/
-helm upgrade --install --version v0.18 lescinskas-lt-descheduler descheduler/descheduler-helm-chart
+helm upgrade --install --version v0.18 --namespace kube-system lescinskas-lt-descheduler descheduler/descheduler-helm-chart
