ca8eed09d5c6feed5794cc28ad4e4fb961d0f55b
Paulius Leščinskas <paulius.lescinskas@gmail.com>
2020-10-02 17:53:50 +0300

    Added descheduler helm chart

---
+++ deploy/helm/descheduler.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# https://github.com/kubernetes-sigs/descheduler
+# Helm chart: https://github.com/kubernetes-sigs/descheduler/blob/master/charts/descheduler/README.md
+# Helm values:     https://github.com/kubernetes-sigs/descheduler/blob/master/charts/descheduler/values.yaml
+helm repo add descheduler https://kubernetes-sigs.github.io/descheduler/
+helm upgrade --install --version v0.18 lescinskas-lt-descheduler descheduler/descheduler-helm-chart
