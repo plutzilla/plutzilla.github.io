31fb99d906af60cbefa580997cbd0c4ae2a1b109
Paulius Leščinskas <paulius.lescinskas@gmail.com>
2020-12-27 17:03:32 +0200

    Provisioning files moved to the separate repo

--- deploy/helm/ingress-controller.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# https://docs.nginx.com/nginx-ingress-controller/installation/installation-with-helm/
-helm repo add nginx-stable https://helm.nginx.com/stable
-helm repo update
-helm upgrade --install -f ./lescinskas-lt-nginx-ingress.values.yaml lescinskas-lt-nginx-ingress nginx-stable/nginx-ingress
